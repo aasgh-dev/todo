a785541df6711275e726255aa5eeacca6d132b46
aasgh <aasgh.devolper@gmail.com>
2026-02-19 16:57:09 +0300

    using admin gate in index , todos pages

--- resources/views/todo/todos.blade.php
+++ resources/views/todo/todos.blade.php
@@ -19,22 +19,20 @@
             <br>
             <div class="col-12 align-self-center">
                 <ul class="list-group">
-                    <!-- loop to show all todo -->
+
                     @foreach ($todos as $todo)
                         <li class="list-group-item">
 
-                            <!-- hyperlink to edit or delete todo -->
                             <a href="{{ route('projects.todos.show', [$project, $todo]) }}"
                                 style="color: cornflowerblue">{{ $todo->name }}</a>
 
-                            <!-- to show who made this todo -->
                             <p>Statu : {{ $todo->status}}</p>
                             <p>create by {{ $todo->user->name}}</p>
-                            @can(Auth::user()->is_admin)
+
+                            @can('admin-only')
                                 <a href="">members : {{ $todo->invites_task()->count() }}</a>
                             @endcan
 
-                            {{-- <a href="{{ route('invites.show',$todo) }}">assign to task</a> --}}
 
                         </li>
                     @endforeach
